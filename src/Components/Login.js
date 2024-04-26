20fe56df9071f88d19c9aefb72c6f845df5eb591
nirowu <rong.cs09@nycu.edu.tw>
2024-04-26 23:20:29 +0800

    fix Login.js <div>

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,6 +3,7 @@ import './Login.css';
 import Navbar from './Navbar';
 import Register from './Register';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = (props) => {
   const [email, setEmail] = useState('')
@@ -10,9 +11,28 @@ const Login = (props) => {
   const [emailError, setEmailError] = useState('')
   const [passwordError, setPasswordError] = useState('')
 
-  const onButtonClick = () => {
-    // You'll update this function later...
-  }
+  // const onButtonClick = () => {
+  //   // You'll update this function later...
+  // }
+
+  const onButtonClick = async (e) => {
+    e.preventDefault();
+    const data = { 
+      "username": email, 
+      "password": password 
+    };
+
+    try {
+      const response = await axios.post('http://127.0.0.1:81/user/login', data);
+      if (response.status === 200) {
+        console.log('登入成功！');
+      } else {
+        console.error('登入失敗！');
+      }
+    } catch (error) {
+      console.error('發生錯誤：', error);
+    }
+  };
 
   return (
     <div className={'mainContainer'}>
@@ -39,7 +59,7 @@ const Login = (props) => {
           <label className={'inputLabel'}>密碼</label>
           <div className={'inputBox'}>
             <input
-              value={email}
+              value={password}
               onChange={(ev) => setPassword(ev.target.value)}
               className={'input'}
             />
@@ -52,9 +72,9 @@ const Login = (props) => {
         <div className={'loginBox'}>
           <input className={'inputButton'} type="button" onClick={onButtonClick} value={'登入'} />
         </div>
+      </div>
       <br />
        <Link to="/register">還不是會員? 註冊新帳號</Link>
-      </div>
     </div>
   )
 }
